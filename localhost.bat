bba734592429e3eba1ad37945c606e324534c6eb
Alpar Duman <83065389+AlparDuman@users.noreply.github.com>
2025-10-09 11:33:55 +0200

    setup enviroment

---
+++ localhost.bat
@@ -0,0 +1,4 @@
+@echo off
+title primes-javascript
+python -m http.server 80
+exit 0
---
+++ primes.class.js
@@ -0,0 +1,22 @@
+/*
+    Copyright (C) 2025 Alpar Duman
+    This file is part of primes-javascript.
+
+    primes-javascript is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 3 as
+    published by the Free Software Foundation.
+
+    primes-javascript is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with primes-javascript. If not, see
+    <https://github.com/AlparDuman/primes-javascript/blob/main/LICENSE>
+    else <https://www.gnu.org/licenses/>.
+*/
+
+class Primes {
+
+}
